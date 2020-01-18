19cb2077103632e66e2d8ea7ff1201930e64bd37
julesep3 <50899932+julesep3@users.noreply.github.com>
2020-01-17 22:23:37 -0600

    error adding 'O' player

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -3,21 +3,17 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 class Square extends React.Component {
+  /*
   constructor(props) {
     super(props);
     this.state = {
       value: null
     };
-  }
+  }*/
   render() {
     return (
-      <button
-        className="square"
-        onClick={() => {
-          this.setState({ value: "X" });
-        }}
-      >
-        {this.state.value}
+      <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
       </button>
     );
   }
@@ -32,7 +28,12 @@ class Board extends React.Component {
   }
 
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
